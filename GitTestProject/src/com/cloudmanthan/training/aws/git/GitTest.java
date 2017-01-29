0584394f965d7ba3a620fec33c777d527f1b5213
amod.kadam <amod.kadam@cloudmanthan.com>
2017-01-29 21:47:29 +0530

    adding gittest

--- GitTestProject/src/com/cloudmanthan/training/aws/git/GitTest.java
+++ GitTestProject/src/com/cloudmanthan/training/aws/git/GitTest.java
@@ -3,7 +3,8 @@ package com.cloudmanthan.training.aws.git;
 public class GitTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+			System.out.println("This is a test");
 
 	}
 
